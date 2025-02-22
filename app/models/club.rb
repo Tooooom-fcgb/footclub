e7699473d706af08b5a221ec69b87007c0f5900c
Tooooom-fcgb <tom.detaevernier@mmibordeaux.com>
2025-02-22 20:33:50 +0100

    BOUTON ( Lorenzo )

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,7 @@
 class Club < ApplicationRecord
+    has_many :players
+    
+    def to_s
+        name
+    end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,8 @@
 class Player < ApplicationRecord
   belongs_to :club
+
+  def to_s
+    surname
+  end
+  
 end
